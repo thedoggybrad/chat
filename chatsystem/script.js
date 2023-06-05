75ed0abe02c6d8223c562b8164dcfe23474afbd6
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-05 14:30:24 +0800

    Update script.js

--- chatsystem/script.js
+++ chatsystem/script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "*************************";
+const API_KEY = "USEYOUROWNIFYOuFORKEDTHISORUSEHIDDENKEYONMYSITE";
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
