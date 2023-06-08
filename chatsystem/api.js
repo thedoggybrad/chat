44218ced5b06501134091e268bfa2aa453c417e1
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-08 08:43:55 +0800

    Create api.js

---
+++ chatsystem/api.js
@@ -0,0 +1,18 @@
+//This is the unecrypted version of the file with no API Keys installed - For local machine use
+
+// Chooses The API Key (Random) - START
+function getRandomItem(arr) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const array = ['API KEY 1', 'API KEY 2', 'AND SO ON...'];
+
+const result = getRandomItem(array);
+// Chooses The API Key (Random) - END
